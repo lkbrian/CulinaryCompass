d76546f10a955e4b1f09e62c78316a293b4aa2c0
lkbrian <lkbrian742@gmail.com>
2024-04-29 13:52:21 +0300

    refactor: removed  the before request third party

--- server/app.py
+++ server/app.py
@@ -5,17 +5,6 @@ from config import app, db, api
 from models import Ingredient, User, Recipe
 
 
-@app.before_request
-def check_if_logged_in():
-    access = [
-        'signup',
-        'login',
-        'check_session'
-    ]
-
-    if (request.endpoint) not in access and (not session.get('user_id')):
-        return {'error': '401 Unauthorized'}, 401
-
 
 class Home(Resource):
     def get(self):
